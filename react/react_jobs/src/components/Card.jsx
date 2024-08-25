31b10d067784da1c903e56a6eff5910096edab41
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-25 16:38:19 -0500

    before react router

--- react/react_jobs/src/components/Card.jsx
+++ react/react_jobs/src/components/Card.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
 
-const Card = ({childern, bg = 'bg-gray-100'}) => {
-  return <div className={`${bg} p-6 rounded-lg shadow-md`}> {childern}
-    </div>;
+const Card = ({ children, bg = 'bg-gray-100' }) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
 export default Card;
+
+// const Card = ({ children, bg = 'bg-gray-100' }) => {
+//   return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
+// };
+// export default Card;
