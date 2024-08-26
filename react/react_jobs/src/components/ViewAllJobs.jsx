8ef6f08f62c9fd5f2067ef7c5cf4930055d606d9
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-26 09:58:57 -0500

    updating a tag to links

--- react/react_jobs/src/components/ViewAllJobs.jsx
+++ react/react_jobs/src/components/ViewAllJobs.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const ViewAllJobs = () => {
   return (
-    <section className="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
-      className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs
-      </a>
-  </section>
+    <section className='m-auto max-w-lg my-10 px-6'>
+      <Link
+        to='/jobs'
+        className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
+      >
+        View All Jobs
+      </Link>
+    </section>
   );
 };
-
-export default ViewAllJobs
+export default ViewAllJobs;
